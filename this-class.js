378d930216e3674675ff174ebf5b5a4928ed29ba
Nirmal Kumar <84436580+Nirmal-Kumar97@users.noreply.github.com>
2022-07-12 16:25:45 +0530

    add

--- this-class.js
+++ this-class.js
@@ -1,16 +1,26 @@
-class Car {
-    constructor(name, year) {
-        this.name = name;
-        this.year = year;
+// class Car {
+//     constructor(name, year) {
+//         this.name = name;
+//         this.year = year;
+//     }
+
+//     age(currentYear) {
+//         return currentYear - this.year;
+//     }
+// }
+
+// let date = new Date();
+// let year = date.getFullYear();
+
+// let car = new Car("Ford", 2014);
+// console.log(car.age(year));
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
     }
-
-    age(currentYear) {
-        return currentYear - this.year;
-    }
-}
-
-let date = new Date();
-let year = date.getFullYear();
-
-let car = new Car("Ford", 2014);
-console.log(car.age(year));
+};
+console.log(person.fullName());
