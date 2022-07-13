54d4d4aff14fe731b8ea4658b2f9ab0a60b1dfcf
Nirmal Kumar <84436580+Nirmal-Kumar97@users.noreply.github.com>
2022-07-13 09:48:07 +0530

    add

--- time.js
+++ time.js
@@ -1,5 +1,24 @@
-for (var i = 0; i < 10; i++) {
-    setTimeout(function () {
-        console.log(i);
-    }, 10);
-}
+// for (var i = 0; i < 10; i++) {
+//     setTimeout(function () {
+//         console.log(i);
+//     }, 10);
+// }
+
+// let a = Math.floor(Math.random() * 10);
+// console.log(a);
+
+let a=
+    {
+        name: "nirmal",
+        id: 1
+    }
+console.log(a.length);
+
+
+// let array = [1, 2, 3, 4];
+// let sum = 0
+
+// for (let i = 0; i < array.length; i++) {
+//     sum += array[i]
+// }
+// console.log(sum);
